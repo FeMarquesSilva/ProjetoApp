856365f9487a82989ca0c2f041ff345a0c28c8b1
Felipe <TADS.FELIPE@outlook.com>
2024-09-06 21:56:45 -0300

    criação do Header

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+type HeaderProps = {
+    title: string
+}
+
+const Header = ({ title }: HeaderProps) => {
+    return (
+        <View>
+            <Text>{title}</Text>
+        </View>
+    );
+}
+
+export default Header;
