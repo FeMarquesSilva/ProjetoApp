c29cdebb41f604873e97018d730034f05b29a5a8
FeMarquesSilva <felipemarques330@gmail.com>
2024-09-07 17:02:37 -0300

    Acrescentando Bichinhos na tela de cadastro

--- app/TelasDoGame/RegistrationScreen.tsx
+++ app/TelasDoGame/RegistrationScreen.tsx
@@ -4,7 +4,10 @@ import { FlatList, Image, Pressable, SafeAreaView, StyleSheet, Text, TextInput, 
 
 const bichinhoImages = [
   { id: 1, source: require('@/assets/images/bichinho.png') },
-  { id: 2, source: require('@/assets/images/bichinho2.png') }
+  { id: 2, source: require('@/assets/images/bichinho2.png') },
+  { id: 3, source: require('@/assets/images/bichinho3.png') },
+  { id: 4, source: require('@/assets/images/bichinho4.png') },
+  { id: 5, source: require('@/assets/images/bichinho5.png') },
 ]
 
 const RegistrationScreen = () => {
