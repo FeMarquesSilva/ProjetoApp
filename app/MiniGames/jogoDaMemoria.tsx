922afe2e78655f16459b0466d0eb53fbb2a8215a
FeMarquesSilva <felipemarques330@gmail.com>
2024-09-07 15:55:00 -0300

    Ajustando o nome dos Cabeçalhos, padroniuzando maiusculo

--- app/MiniGames/jogoDaMemoria.tsx
+++ app/MiniGames/jogoDaMemoria.tsx
@@ -4,8 +4,7 @@ import { Text, View } from "react-native";
 const jogoDaMemoria = () => {
     return (
         <View>
-            <Header title="Jogo da Memoria"></Header>
-            <Text>Jogo da memória</Text>
+            <Header title="JOGO DA MEMÓRIA"></Header>
         </View>
     );
 }
