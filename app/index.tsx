4c00a5d0c548a5fe57b327015aefba8adcfec7f1
FeMarquesSilva <felipemarques330@gmail.com>
2024-09-08 15:40:32 -0300

    Craindo o scopo de como vai ficar a tela de carregamento

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import { router } from "expo-router";
 import { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Index = () => {
 
     const [progress, setProgress] = useState(0);
     const [loadingComplete, setLoadingComplete] = useState(false);
-    
+
     // Simula o carregamento
     useEffect(() => {
         const interval = setInterval(() => {
@@ -23,11 +25,29 @@ const Index = () => {
         return () => clearInterval(interval);
     }, []);
 
+    //Função para ao carregar 100% o botão "Jogar" fique acessivel para o jogador;
+    const handlePlayPress = () => {
+        if (loadingComplete) {
+          router.navigate("/TelasDoGame");
+        }
+      };
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Essa tela deverá exibir a barra de carregamento do jogo!</Text>
-            <Button title="Tela inicial" onPress={() => router.navigate("/TelasDoGame")} />
+        <SafeAreaView style={styles.container}>
+        <View>
+          <Text>Carregando...</Text>
+          <View>
+            <View />
+          </View>
+          <Text>{Math.round(progress)}%</Text>
+          <TouchableOpacity
+            onPress={handlePlayPress}
+            disabled={!loadingComplete}
+          >
+            <Text>Jogar</Text>
+          </TouchableOpacity>
         </View>
+      </SafeAreaView>
     );
 }
 
@@ -37,12 +57,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        color: '#000'
+        alignItems: 'center',
+        backgroundColor: '#FF8433',
+      },
 
-    },
-    text: {
-        fontSize: 30,
-        color: '#000',
-        textAlign: 'center',
-    }
 })
