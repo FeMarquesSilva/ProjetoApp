43333b2dd3ff6ca17a273918341a9a00c4d88ef6
FeMarquesSilva <felipemarques330@gmail.com>
2024-09-09 22:27:04 -0300

    Adicionando a função de apagar um tamagochi pela tela de listagem

--- app/TelasDoGame/index.tsx
+++ app/TelasDoGame/index.tsx
@@ -1,167 +1,207 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image, FlatList } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  Button,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { router, useFocusEffect } from "expo-router";
 import Header from "@/components/Header";
 import { useTodoDatabase } from "../database/todoService";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 type TamagochiList = {
-    id: number;
-    name: string;
-    image: number;
-    hunger: number;
-    sleep: number;
-    fun: number;
-    status: string;
+  id: number;
+  name: string;
+  image: number;
+  hunger: number;
+  sleep: number;
+  fun: number;
+  status: string;
 };
 
 const bichinhoImages = [
-    { id: 1, source: require('@/assets/images/bichinho.png') },
-    { id: 2, source: require('@/assets/images/bichinho2.png') },
-    { id: 3, source: require('@/assets/images/bichinho3.png') },
-    { id: 4, source: require('@/assets/images/bichinho4.png') },
-    { id: 5, source: require('@/assets/images/bichinho5.png') },
+  { id: 1, source: require("@/assets/images/bichinho.png") },
+  { id: 2, source: require("@/assets/images/bichinho2.png") },
+  { id: 3, source: require("@/assets/images/bichinho3.png") },
+  { id: 4, source: require("@/assets/images/bichinho4.png") },
+  { id: 5, source: require("@/assets/images/bichinho5.png") },
 ];
 
 const Index = () => {
-    const [tamagochiList, setTamagochiList] = useState<TamagochiList[]>([]);
-    const { getTamagochi, alterTamagochi } = useTodoDatabase();
+  const [tamagochiList, setTamagochiList] = useState<TamagochiList[]>([]);
+  const { getTamagochi, alterTamagochi, deleteTamagochiById } = useTodoDatabase();
 
-    // Função para carregar as informações do banco e armazenar no estado
-    const list = async () => {
+  // Função para carregar as informações do banco e armazenar no estado
+  const list = async () => {
+    try {
+      const response: TamagochiList[] = await getTamagochi();
+      setTamagochiList(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Chama a função de listagem quando o componente for montado
+  useFocusEffect(() => {
+    list();
+  });
+
+  // Atualiza os atributos a cada 3 segundos
+  useEffect(() => {
+    const atualizarAtributos = async () => {
+      try {
+        const tamagochis = await getTamagochi();
+        const updatedTamagochis = tamagochis.map((tamagochi) => {
+          const updatedHunger = Math.max(tamagochi.hunger - 1, 0);
+          const updatedSleep = Math.max(tamagochi.sleep - 1, 0);
+          const updatedFun = Math.max(tamagochi.fun - 1, 0);
+
+          const total = updatedHunger + updatedSleep + updatedFun;
+          let status = "";
+          if (total === 0) status = "morto";
+          else if (total <= 50) status = "crítico";
+          else if (total <= 100) status = "muito triste";
+          else if (total <= 150) status = "triste";
+          else if (total <= 200) status = "ok";
+          else if (total <= 250) status = "bem";
+          else status = "muito bem";
+
+          return {
+            ...tamagochi,
+            hunger: updatedHunger,
+            sleep: updatedSleep,
+            fun: updatedFun,
+            status,
+          };
+        });
+
+        await Promise.all(
+          updatedTamagochis.map((tamagochi) => alterTamagochi(tamagochi))
+        );
+
+        setTamagochiList(updatedTamagochis); // Atualiza a lista no estado
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const interval = setInterval(atualizarAtributos, 30000); // 30s
+
+    return () => clearInterval(interval);
+  }, [getTamagochi, alterTamagochi]);
+
+  // Renderiza cada tamagochi da lista
+  const renderItem = ({ item }: { item: TamagochiList }) => {
+    const imageSource = bichinhoImages.find(
+      (img) => img.id === Number(item.image)
+    )?.source;
+
+    //Função para ao precionar o botão de 'trash' o tamagochi será apagado da tabela no banco de dados de acordo com o id;
+    const removeTamagochi = async ( id: number) => {
+        alert(`Deseja realmente remover seu tamagochi ${item.name} permanemtemente?`)
         try {
-            const response: TamagochiList[] = await getTamagochi();
-            setTamagochiList(response);
+            await deleteTamagochiById({id})
         } catch (error) {
             console.error(error);
         }
-    };
-
-    // Chama a função de listagem quando o componente for montado
-    useFocusEffect(() => {
-        list();
-    });
-
-    // Atualiza os atributos a cada 3 segundos
-    useEffect(() => {
-        const atualizarAtributos = async () => {
-            try {
-                const tamagochis = await getTamagochi();
-                const updatedTamagochis = tamagochis.map(tamagochi => {
-                    const updatedHunger = Math.max(tamagochi.hunger - 1, 0);
-                    const updatedSleep = Math.max(tamagochi.sleep - 1, 0);
-                    const updatedFun = Math.max(tamagochi.fun - 1, 0);
-
-                    const total = updatedHunger + updatedSleep + updatedFun;
-                    let status = '';
-                    if (total === 0) status = 'morto';
-                    else if (total <= 50) status = 'crítico';
-                    else if (total <= 100) status = 'muito triste';
-                    else if (total <= 150) status = 'triste';
-                    else if (total <= 200) status = 'ok';
-                    else if (total <= 250) status = 'bem';
-                    else status = 'muito bem';
-
-                    return { ...tamagochi, hunger: updatedHunger, sleep: updatedSleep, fun: updatedFun, status };
-                });
-
-                await Promise.all(updatedTamagochis.map(tamagochi =>
-                    alterTamagochi(tamagochi)
-                ));
-
-                setTamagochiList(updatedTamagochis); // Atualiza a lista no estado
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        const interval = setInterval(atualizarAtributos, 30000); // 30s
-
-        return () => clearInterval(interval);
-    }, [getTamagochi, alterTamagochi]);
-
-    // Renderiza cada tamagochi da lista
-    const renderItem = ({ item }: { item: TamagochiList }) => {
-        const imageSource = bichinhoImages.find(img => img.id === Number(item.image))?.source;
-
-        return (
-            //Retorno a visualização dos cardas com o opção de touch para ir nos detalhes dos bichinhos;
-            <TouchableOpacity onPress={() => { router.push(`/TelasDoGame/detailsScreen?id=${item.id}&name=${item.name}&image=${item.image}&hunger=${item.hunger}&sleep=${item.sleep}&fun=${item.fun}`) }}>
-                <View style={styles.card}>
-                    <Image source={imageSource} style={styles.image} />
-                    <View style={styles.info}>
-                        <Text style={styles.name}>{item.name}</Text>
-                        <Text>Fome: {item.hunger}</Text>
-                        <Text>Sono: {item.sleep}</Text>
-                        <Text>Diversão: {item.fun}</Text>
-                        <Text>Status: {item.status}</Text>
-                    </View>
-                </View>
-            </TouchableOpacity>
-        );
-    };
+    }
 
     return (
-        <SafeAreaView style={styles.container}>
-            <Header title="LISTAGEM DOS BICHINHOS"></Header>
-            <FlatList
-                data={tamagochiList}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.id.toString()}
-                contentContainerStyle={styles.list}
-            />
-            <TouchableOpacity style={styles.button} onPress={() => { router.push("/TelasDoGame/registrationScreen") }}>
-                <Ionicons name="add-outline" size={24} color="black" />
-            </TouchableOpacity>
-        </SafeAreaView>
+      //Retorno a visualização dos cardas com o opção de touch para ir nos detalhes dos bichinhos;
+      <TouchableOpacity
+        onPress={() => {
+          router.push(
+            `/TelasDoGame/detailsScreen?id=${item.id}&name=${item.name}&image=${item.image}&hunger=${item.hunger}&sleep=${item.sleep}&fun=${item.fun}`
+          );
+        }}
+      >
+        <View style={styles.card}>
+          <Image source={imageSource} style={styles.image} />
+          <View style={styles.info}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text>Fome: {item.hunger}</Text>
+            <Text>Sono: {item.sleep}</Text>
+            <Text>Diversão: {item.fun}</Text>
+            <Text>Status: {item.status}</Text>
+          </View>
+          <TouchableOpacity onPress={() => {removeTamagochi(item.id)}}>
+            <Ionicons name='trash' size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
     );
-}
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header title="LISTAGEM DOS BICHINHOS"></Header>
+      <FlatList
+        data={tamagochiList}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.list}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          router.push("/TelasDoGame/registrationScreen");
+        }}
+      >
+        <Ionicons name="add-outline" size={24} color="black" />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
 
 export default Index;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#FF8433"
-    },
-    list: {
-        padding: 20,
-    },
-    card: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: '#f9f9f9',
-        padding: 10,
-        marginBottom: 10,
-        borderRadius: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 3
-    },
-    image: {
-        width: 100,
-        height: 100,
-        borderRadius: 10,
-        marginRight: 10,
-    },
-    info: {
-        flex: 1,
-    },
-    name: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 5
-    },
-    button: {
-        flexDirection: 'row',
-        backgroundColor: '#f13601',
-        padding: 20,
-        borderRadius: 10,
-        position: 'absolute',
-        bottom: 30,
-        right: 20,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#FF8433",
+  },
+  list: {
+    padding: 20,
+  },
+  card: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f9f9f9",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  info: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  button: {
+    flexDirection: "row",
+    backgroundColor: "#f13601",
+    padding: 20,
+    borderRadius: 10,
+    position: "absolute",
+    bottom: 30,
+    right: 20,
+  },
 });
--- app/database/todoService.ts
+++ app/database/todoService.ts
@@ -1,6 +1,86 @@
-import { useSQLiteContext } from "expo-sqlite"
+import { useSQLiteContext } from "expo-sqlite";
 
 type TamagochiList = {
+  id: number;
+  name: string;
+  image: number;
+  hunger: number;
+  sleep: number;
+  fun: number;
+  status: string;
+};
+
+// Todas as funções de banco, deverá ser criada neste arquivo, para ser exportadora, e importado somente nos locais necessários.
+export function useTodoDatabase() {
+  const database = useSQLiteContext();
+
+  //Funçã para salvar os bichinhos na tabela do banco de dados chamada: tamagchis
+  async function saveTamagochi({
+    name,
+    image,
+    hunger,
+    sleep,
+    fun,
+    status,
+  }: {
+    name: string;
+    image: number;
+    hunger: number;
+    sleep: number;
+    fun: number;
+    status: string;
+  }) {
+    const query =
+      await database.prepareAsync(`INSERT INTO tamagchis (name, image, hunger, sleep, fun, status) 
+            VALUES ($name, $image, $hunger, $sleep, $fun, $status);`);
+    try {
+      await query.executeAsync({
+        $name: name,
+        $image: image,
+        $hunger: hunger,
+        $sleep: sleep,
+        $fun: fun,
+        $status: status,
+      });
+    } catch (error) {
+      throw error;
+    } finally {
+      await query.finalizeAsync();
+    }
+  }
+
+  async function getTamagochi() {
+    try {
+      const response = await database.getAllAsync<TamagochiList>(
+        `SELECT * FROM tamagchis;`
+      );
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  //criando função para deletar a tabela de tamagochi caso necessário;
+  async function deleteTamagochi() {
+    try {
+      const query = await database.prepareAsync(`DROP TABLE tamagchis;`);
+      await query.executeAsync();
+      await query.finalizeAsync();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  //Função para atualizar as informações:
+  async function alterTamagochi({
+    id,
+    name,
+    image,
+    hunger,
+    sleep,
+    fun,
+    status,
+  }: {
     id: number;
     name: string;
     image: number;
@@ -8,56 +88,40 @@ type TamagochiList = {
     sleep: number;
     fun: number;
     status: string;
-}
-
-// Todas as funções de banco, deverá ser criada neste arquivo, para ser exportadora, e importado somente nos locais necessários.
-export function useTodoDatabase() {
-    const database = useSQLiteContext()
-
-    //Funçã para salvar os bichinhos na tabela do banco de dados chamada: tamagchis
-    async function saveTamagochi({ name, image, hunger, sleep, fun, status }: { name: string, image: number, hunger: number, sleep: number, fun: number, status: string }) {
-        const query = await database.prepareAsync(`INSERT INTO tamagchis (name, image, hunger, sleep, fun, status) 
-            VALUES ($name, $image, $hunger, $sleep, $fun, $status);`)
-        try {
-            await query.executeAsync({ $name: name, $image: image, $hunger: hunger, $sleep: sleep, $fun: fun, $status: status })
-        } catch (error) {
-            throw error;
-        } finally {
-            await query.finalizeAsync()
-        }
+  }) {
+    const query = await database.prepareAsync(
+      `UPDATE tamagchis SET name = $name, image = $image, hunger = $hunger, sleep = $sleep, fun = $fun, status = $status WHERE id = $id;`
+    );
+    try {
+      await query.executeAsync({
+        $id: id,
+        $name: name,
+        $image: image,
+        $hunger: hunger,
+        $sleep: sleep,
+        $fun: fun,
+        $status: status,
+      });
+    } catch (error) {
+      throw error;
+    } finally {
+      await query.finalizeAsync();
     }
+  }
 
-    async function getTamagochi() {
-        try {
-            const response = await database.getAllAsync<TamagochiList>(`SELECT * FROM tamagchis;`)
-            return response
-        } catch (error) {
-            throw error;
-        }
+  //Função para deletar o tamagochi da tabela de acordo com o ID:
+  async function deleteTamagochiById({ id }: { id: number }) {
+    const query = await database.prepareAsync(
+      `DELETE FROM tamagchis WHERE id = $id`
+    );
+    try {
+      await query.executeAsync({ $id: id });
+    } catch (error) {
+      throw error;
+    } finally {
+      await query.finalizeAsync();
     }
+  }
 
-    //criando função para deletar a tabela de tamagochi caso necessário;
-    async function deleteTamagochi() {
-        try {
-            const query = await database.prepareAsync(`DROP TABLE tamagchis;`)
-            await query.executeAsync()
-            await query.finalizeAsync()
-        } catch (error) {
-            throw error;
-        }
-    }
-
-    //Função para atualizar as informações: 
-    async function alterTamagochi({ id, name, image, hunger, sleep, fun, status }: { id: number, name: string, image: number, hunger: number, sleep: number, fun: number, status: string }) {
-        const query = await database.prepareAsync(`UPDATE tamagchis SET name = $name, image = $image, hunger = $hunger, sleep = $sleep, fun = $fun, status = $status WHERE id = $id;`);
-        try {
-            await query.executeAsync({ $id: id, $name: name, $image: image, $hunger: hunger, $sleep: sleep, $fun: fun, $status: status });
-        } catch (error) {
-            throw error;
-        } finally {
-            await query.finalizeAsync();
-        }
-    }
-
-    return { saveTamagochi, getTamagochi, deleteTamagochi, alterTamagochi }
+  return { saveTamagochi, getTamagochi, deleteTamagochi, alterTamagochi, deleteTamagochiById };
 }
