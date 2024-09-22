2efa2795577a154dde0c498c3961122ab31cff9d
FeMarquesSilva <felipemarques330@gmail.com>
2024-09-22 12:09:12 -0300

    Adicionando novos bichinhos

--- app/functions/services.ts
+++ app/functions/services.ts
@@ -1,11 +1,18 @@
 export function todoFunctions() {
     // Array de objetos com imagens dos bichinhos:
     const tamagochiImages = [
-        { id: 1, source: require("@/assets/images/bichinho.png") },
+        { id: 1, source: require("@/assets/images/bichinho1.png") },
         { id: 2, source: require("@/assets/images/bichinho2.png") },
         { id: 3, source: require("@/assets/images/bichinho3.png") },
         { id: 4, source: require("@/assets/images/bichinho4.png") },
         { id: 5, source: require("@/assets/images/bichinho5.png") },
+        { id: 6, source: require("@/assets/images/bichinho6.png") },
+        { id: 7, source: require("@/assets/images/bichinho7.png") },
+        { id: 8, source: require("@/assets/images/bichinho8.png") },
+        { id: 9, source: require("@/assets/images/bichinho9.png") },
+        { id: 10, source: require("@/assets/images/bichinho10.png") },
+        { id: 11, source: require("@/assets/images/bichinho11.png") },
+        { id: 12, source: require("@/assets/images/bichinho12.png") },
       ];
 
     return { tamagochiImages }
