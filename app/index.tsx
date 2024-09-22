17c0b4f2d6d2255de871b18f18e177c6abd2ec29
FeMarquesSilva <felipemarques330@gmail.com>
2024-09-22 09:32:08 -0300

    Refazendo os comentarios da tela de carregamento e deixando o código mais 'clean'

--- app/index.tsx
+++ app/index.tsx
@@ -1,69 +1,56 @@
 import { router } from "expo-router";
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  SafeAreaView,
-} from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView } from "react-native";
 
 const Index = () => {
-  // Define um estado 'progress' com valor inicial 0, que armazenará o progresso da barra de carregamento;
+  //Criamos os estados "progress" e "loadingComplete" para validar o progresso de carregamento e sua conclusão:
   const [progress, setProgress] = useState(0);
-
-  // Define um estado 'loadingComplete' para verificar se o carregamento foi concluído;
   const [loadingComplete, setLoadingComplete] = useState(false);
 
   // Simula o processo de carregamento usando o hook useEffect;
   useEffect(() => {
-    // Define um intervalo que será executado a cada 30 milissegundos;
     const interval = setInterval(() => {
-      // Se o progresso atingir ou ultrapassar 100, o carregamento é concluído;
       setProgress((prevProgress) => {
-        // Limpa o intervalo quando o carregamento termina;
+        // Quando o "progress" estiver em 100, limpamos o intervalo determinamos que o loading finalizou:
         if (prevProgress >= 100) {
-          // Seta 'loadingComplete' como true;
+          // Limpa o intervalo quando o carregamento termina;
           clearInterval(interval);
           setLoadingComplete(true);
-          // Garante que o valor de progresso não passe de 100;
           return 100;
         }
-        // Incrementa o progresso em 1 a cada ciclo;
+
         return prevProgress + 1;
       });
-    }, 30); // Define o intervalo de tempo para o ciclo de atualização do progresso (30ms);
+    }, 30); // Intervalo de tempo para o ciclo de atualização do progresso (30ms);
     // Função de limpeza para limpar o intervalo quando o componente for desmontado;
     return () => clearInterval(interval);
   }, []);
 
-  // Função que permite a navegação para a tela de jogo quando o carregamento atinge 100%;
-  const liberarStart = () => {
+  // Função que permite a navegação para a tela de jogo quando o carregamento atingir 100%:
+  const start = () => {
     if (loadingComplete) {
-      // Navega para a rota "/TelasDoGame" usando o Expo Router;
       router.navigate("/TelasDoGame");
     }
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.title}>Carregando...</Text>
+        <Text style={styles.containerText}>Carregando...</Text>
         <View style={styles.progressContainer}>
+          {/* Exibmos uma barra de progresso com a largura dinâmica, baseada no valor da variável "progress": */}
           <View style={[styles.progressBar, { width: `${progress}%` }]} />
         </View>
-        <Text style={styles.progressText}>{Math.round(progress)}%</Text>
+        {/*Exibimos o "%" do progresso porem de forma arredondada:*/}
+        <Text style={styles.containerText}>{Math.round(progress)}%</Text>
+        {/*Colocamos o botçao jogar de forma dinamica, onde enquanto o progresso não estiver completo,
+        o play não libera e se mantem com fundo cinza:*/}
         <TouchableOpacity
-          style={[
-            styles.button,
-            { backgroundColor: loadingComplete ? "#28a745" : "#6c757d" },
-          ]}
-          onPress={liberarStart}
+          style={[styles.button,{ backgroundColor: loadingComplete ? "#28a745" : "#6c757d" }]}
+          onPress={start}
           disabled={!loadingComplete}
         >
-          <Text style={styles.buttonText}>Jogar</Text>
+          <Text style={styles.containerText}>Jogar</Text>
         </TouchableOpacity>
-      </View>
     </SafeAreaView>
   );
 };
@@ -71,20 +58,19 @@ const Index = () => {
 export default Index;
 
 const styles = StyleSheet.create({
+  //Estilos gerais da página:
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#FF8433",
   },
-  content: {
-    alignItems: "center",
-  },
-  title: {
+  //Estilos para os textos:
+  containerText: {
     fontSize: 24,
-    marginBottom: 20,
     color: "#fff",
   },
+  //Estilo da barra de progreço com ocupação de 80% da tela:
   progressContainer: {
     width: "80%",
     height: 20,
@@ -93,21 +79,16 @@ const styles = StyleSheet.create({
     overflow: "hidden",
     marginBottom: 20,
   },
+  /*Estilo da barra de progresso de forma dinâmica preenchendo na
+  cor verde:*/
   progressBar: {
     height: "100%",
     backgroundColor: "#28a745",
   },
-  progressText: {
-    fontSize: 18,
-    color: "#fff",
-    marginBottom: 20,
-  },
+  //Estilo do botão:
   button: {
-    padding: 15,
+    padding: 14,
     borderRadius: 5,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 18,
+    marginTop: 60
   },
 });
